25422b25dce2d979e0289c871856ab995c3611d3
Yauhen Zhur <evgeni1897@gmail.com>
2023-09-10 22:11:26 +0300

    add save quips to db

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from home.forms import NewQuipForm
+from main.models import Quip
+from django.contrib import messages
+
 
 
 def home_request(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = NewQuipForm(request.POST)
+        text = form.data['text']
+        quip = Quip(text=text, user = request.user)
+        quip.save()
+        messages.success(request, 'Quip posted!')
+
     form = NewQuipForm()
     return render(
         request=request,
