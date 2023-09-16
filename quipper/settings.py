e9b8c5ba84d796ec84879c7149519deb13541501
Yauhen Zhur <evgeni1897@gmail.com>
2023-09-16 18:57:42 +0300

    update bootstrap

--- quipper/settings.py
+++ quipper/settings.py
@@ -39,16 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    "crispy_bootstrap4",
+    "crispy_bootstrap5",
     'users',
     'home',
     'main',
 
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
