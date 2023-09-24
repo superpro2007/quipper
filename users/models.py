3947744ce9dd821988775598c84437813c56c1b7
Yauhen Zhur <evgeni1897@gmail.com>
2023-09-24 18:50:00 +0300

    add following model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import UniqueConstraint
 
-# Create your models here.
+
+class Following(models.Model):
+    from_user = models.ForeignKey(
+        User, null=False, on_delete=models.CASCADE, related_name="from_following"
+    )
+    to_user = models.ForeignKey(
+        User, null=False, on_delete=models.CASCADE, related_name="to_following"
+    )
+
+    def __str__(self):
+        return f"{self.from_user} follows {self.to_user}"
+
+    class Meta:
+        constraints = (
+            UniqueConstraint(
+                name="unique_from_user_to_user",
+                fields=["from_user", "to_user"],
+            ),
+        )
