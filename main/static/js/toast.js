e26d3a7b6a794cbba088d72c640f8e5656a0d849
Yauhen Zhur <evgeni1897@gmail.com>
2023-10-22 19:37:49 +0300

    test all scripts in one file

--- main/static/js/toast.js
+++ main/static/js/toast.js
@@ -15,3 +15,35 @@ function fade_alerts() {
 window.addEventListener("DOMContentLoaded", (event) => {
   fade_alerts();
 });
+
+$(".follow").click(function () {
+  var userid;
+  userid = $(this).attr("userid");
+  var path;
+  path = "/user/" + userid + "/follow";
+  $.get(path, function (is_following) {
+    var buttonid;
+    buttonid = "#follow_" + userid;
+    if (is_following == "True") {
+      $(buttonid).html("Unfollow");
+    } else {
+      $(buttonid).html("Follow");
+    }
+  });
+});
+
+$(".like").click(function () {
+  var postid;
+  postid = $(this).attr("postid");
+  var path;
+  path = "/quip/" + postid + "/like";
+  $.get(path, function (likes_amount) {
+    var buttonid;
+    buttonid = "#quip_" + postid;
+    if (likes_amount == 0) {
+      $(buttonid).html("🤍 Like");
+    } else {
+      $(buttonid).html("❤️ " + likes_amount);
+    }
+  });
+});
