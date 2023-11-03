f5e557d46c7c61a9b0975f09a84ed43f04b62660
Yauhen Zhur <evgeni1897@gmail.com>
2023-11-03 20:06:50 +0300

    fix follow button from followers list

--- home/templatetags/filters.py
+++ home/templatetags/filters.py
@@ -1,6 +1,12 @@
 from django.template.defaulttags import register
+from django.contrib.auth.models import User
+from users.models import Following
 
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def is_follower(maybe_follower: User, maybe_followed: User) -> bool:
+    return Following.objects.filter(from_user=maybe_follower,to_user=maybe_followed).exists()
